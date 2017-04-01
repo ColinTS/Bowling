55da1233a75d752050695ad1601d8aab50299c3b
ctsygiel <ctsygiel@gmail.com>
2017-03-31 14:48:07 -1000

    all tests passing

--- bowling.js
+++ bowling.js
@@ -1,17 +1,28 @@
 module.exports = (bowls) => {
   var score = 0;
 
-  for(var i = 0; i < bowls.length; i++){
-    if(bowls[i].pinCount1 === 10){
-      score += bowls[i].pinCount1 + ((bowls[i + 1].pinCount1 + bowls[i + 1].pinCount2));
-    }
-    if(bowls[i].pinCount1 + bowls[i].pinCount2 < 10){
-      score += bowls[i].pinCount1 + bowls[i].pinCount2;
-    }
-    if(bowls[i].pinCount1 + bowls[i].pinCount2 === 10){
-      score += bowls[i].pinCount1 + bowls[i].pinCount2 + bowls[i + 1].pinCount1;
+  function strikeLastFrame(){
+      score += bowls[9].pinCount1 + bowls[10].pinCount1 + bowls[10].pinCount2;
     }
 
+  for(var i = 0; i < bowls.length; i++){
+    //strike on last frame
+    if (bowls[9].pinCount1 === 10 && i === 9){
+      strikeLastFrame();
+      break;
+    }
+      //strike
+      else if (bowls[i].pinCount1 === 10){
+      score += bowls[i].pinCount1 + ((bowls[i + 1].pinCount1 + bowls[i + 1].pinCount2));
+    }
+    //spare
+    else if(bowls[i].pinCount1 + bowls[i].pinCount2 === 10){
+      score += bowls[i].pinCount1 + bowls[i].pinCount2 + bowls[i + 1].pinCount1;
+    }
+    //normal roll
+    else if(bowls[i].pinCount1 + bowls[i].pinCount2 < 10){
+      score += bowls[i].pinCount1 + bowls[i].pinCount2;
+    }
   }
   return score;
 };
--- tests/bowling.spec.js
+++ tests/bowling.spec.js
@@ -9,19 +9,6 @@ describe('bowling function', () => {
     expect(bowling).to.be.a('function');
   });
 
-  it('should return a score equal to the sum of the pins if less than 10 pins are knocked down', () => {
-    expect(bowling([{pinCount1: 5, pinCount2: 3}])).to.equal(8);
-  });
-
-  it('should return a spare score', () => {
-    expect(bowling([{pinCount1: 5, pinCount2: 5},
-      {pinCount1: 5, pinCount2: 3}])).to.equal(23);
-  });
-
-  it('should return a strike score', () => {
-    expect(bowling([{pinCount1: 10},{pinCount1: 5, pinCount2: 3}])).to.equal(26);
-  });
-
   it('should return a correct total score with no spares or strikes', () => {
     expect(bowling([{pinCount1: 4, pinCount2: 5},
       {pinCount1: 4, pinCount2: 5},
@@ -47,6 +34,37 @@ describe('bowling function', () => {
       {pinCount1: 4, pinCount2: 5},
       {pinCount1: 4, pinCount2: 5}])).to.equal(105);
   });
+
+  it('should return a correct total score with spare as 10th frame', () => {
+    expect(bowling([{pinCount1: 10},
+      {pinCount1: 4, pinCount2: 5},
+      {pinCount1: 4, pinCount2: 5},
+      {pinCount1: 5, pinCount2: 5},
+      {pinCount1: 4, pinCount2: 5},
+      {pinCount1: 4, pinCount2: 5},
+      {pinCount1: 4, pinCount2: 5},
+      {pinCount1: 4, pinCount2: 5},
+      {pinCount1: 4, pinCount2: 5},
+      {pinCount1: 5, pinCount2: 5},
+      {pinCount1: 5}])).to.equal(111); /*bonus roll for spare*/
+  });
+
+  it('should return a correct total score with strike as 10th frame', () => {
+    expect(bowling([{pinCount1: 10},
+      {pinCount1: 4, pinCount2: 5},
+      {pinCount1: 4, pinCount2: 5},
+      {pinCount1: 5, pinCount2: 5},
+      {pinCount1: 4, pinCount2: 5},
+      {pinCount1: 4, pinCount2: 5},
+      {pinCount1: 4, pinCount2: 5},
+      {pinCount1: 4, pinCount2: 5},
+      {pinCount1: 4, pinCount2: 5},
+      {pinCount1: 10},
+      {pinCount1: 5, pinCount2: 4}])).to.equal(115); /*bonus roll for spare*/
+  });
+
+
+
 
 
 
